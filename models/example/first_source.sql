2a511ef7db1549165e4f47000e45391a208ebe35
reshamguru123@gmail.com <reshamguru123@gmail.com>
2022-01-26 20:11:31 +0000

    Second commit

---
+++ models/example/first_source.sql
@@ -0,0 +1,13 @@
+Select
+    c.c_custkey,
+    c.c_name,
+    c.c_nationkey as nation,
+    sum(o.o_totalprice) as total_order_price
+from {{ source('sample','customer') }} c
+LEFT JOIN {{ source('sample','orders') }} o
+ON c.c_custkey=o.o_custkey
+
+group by 
+    c.c_custkey,
+    c.c_name,
+    c.c_nationkey
